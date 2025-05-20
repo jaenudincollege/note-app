9138107975df5fea53e3565d308a9d1b152dd826
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2025-05-20 11:49:45 +0700

    Configure react routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,20 @@
+import { Routes, Route } from "react-router";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import Navbar from "./components/Navbar";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/createnote" element={<CreatePage />} />
+        <Route path="/note/:id" element={<CreatePage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
