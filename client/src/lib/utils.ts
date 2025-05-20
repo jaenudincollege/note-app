d0cb24080ea99e6779ba2cf66d43fe4deeb5c973
Jaenudin Firdaus <2022110103@students.uigm.ac.id>
2025-05-20 20:26:23 +0700

    delete client folder from mern branch

--- client/src/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-const formatDate = (date: Date) => {
-  return date.toLocaleDateString("en-ID", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
-};
-
-export default formatDate;
